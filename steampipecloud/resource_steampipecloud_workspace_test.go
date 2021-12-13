72eacf4a9d830b2c200444c910b64ec079fb2c1d
Subhajit Mondal <38218418+Subhajit97@users.noreply.github.com>
2021-12-13 20:25:09 +0530

    Add acceptance test for resource `steampipecloud_workspace` (#8)

---
+++ steampipecloud/resource_steampipecloud_workspace_test.go
@@ -0,0 +1,107 @@
+package steampipecloud
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+// test suites
+func TestAccUserWorkspace_Basic(t *testing.T) {
+	resourceName := "steampipecloud_workspace.test"
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckUserWorkspaceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccUserWorkspaceConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckUserWorkspaceExists("steampipecloud_workspace.test"),
+					resource.TestCheckResourceAttr(
+						"steampipecloud_workspace.test", "handle", "terraformtest"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccUserWorkspaceUpdateHandleConfig(),
+				Check: resource.TestCheckResourceAttr(
+					"steampipecloud_workspace.test", "handle", "terraformtestworkspace"),
+			},
+		},
+	})
+}
+
+// configs
+func testAccUserWorkspaceConfig() string {
+	return `
+resource "steampipecloud_workspace" "test" {
+	handle = "terraformtest"
+}
+`
+}
+
+func testAccUserWorkspaceUpdateHandleConfig() string {
+	return `
+resource "steampipecloud_workspace" "test" {
+	handle = "terraformtestworkspace"
+}
+`
+}
+
+// helper functions
+func testAccCheckUserWorkspaceExists(resource string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[resource]
+		if !ok {
+			return fmt.Errorf("not found: %s", resource)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no Record ID is set")
+		}
+		client := testAccProvider.Meta().(*SteampipeClient)
+
+		// Get user handle
+		userData, _, userErr := client.APIClient.UsersApi.GetActor(context.Background()).Execute()
+		if userErr != nil {
+			return fmt.Errorf("error fetching user handle. %s", userErr)
+		}
+
+		_, _, err := client.APIClient.UserWorkspacesApi.GetUserWorkspace(context.Background(), userData.Handle, rs.Primary.ID).Execute()
+		if err != nil {
+			return fmt.Errorf("error fetching item with resource %s. %s", resource, err)
+		}
+		return nil
+	}
+}
+
+func testAccCheckUserWorkspaceDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*SteampipeClient)
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type == "steampipecloud_workspace" {
+			// Get user handle
+			userData, _, userErr := client.APIClient.UsersApi.GetActor(context.Background()).Execute()
+			if userErr != nil {
+				return fmt.Errorf("error fetching user handle. %s", userErr)
+			}
+
+			_, r, err := client.APIClient.UserWorkspacesApi.GetUserWorkspace(context.Background(), userData.Handle, rs.Primary.ID).Execute()
+			if err == nil {
+				return fmt.Errorf("alert still exists")
+			}
+
+			if r.StatusCode != 404 {
+				return fmt.Errorf("expected 'no content' error, got %s", err)
+			}
+		}
+	}
+
+	return nil
+}
