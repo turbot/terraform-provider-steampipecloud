cb016926cad4a181782c77b0841a9d7b5340a599
Subhajit Mondal <38218418+Subhajit97@users.noreply.github.com>
2021-12-15 20:36:48 +0530

    Add resource steampipecloud_organization_member (#18)

---
+++ steampipecloud/resource_steampipecloud_organization_member.go
@@ -0,0 +1,366 @@
+package steampipecloud
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/turbot/go-kit/types"
+	steampipe "github.com/turbot/steampipe-cloud-sdk-go"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceOrganizationMember() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceOrganizationMemberCreate,
+		ReadContext:   resourceOrganizationMemberRead,
+		DeleteContext: resourceOrganizationMemberDelete,
+		UpdateContext: resourceOrganizationMemberUpdate,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"user_handle": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"email"},
+			},
+			"role": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"email": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"user_handle"},
+			},
+			"association_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"organization_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"updated_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"user_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"version_id": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"display_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+// CRUD functions
+
+func resourceOrganizationMemberCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+	client := meta.(*SteampipeClient)
+
+	// Return if `organization` is not given in config
+	_, orgHandle := isUserConnection(client)
+	if orgHandle == "" {
+		return diag.Errorf("failed to get organization. Please set 'organization' in provider config")
+	}
+
+	// Create request
+	req := steampipe.InviteOrgUserRequest{
+		Role: d.Get("role").(string),
+	}
+
+	if value, ok := d.GetOk("user_handle"); ok {
+		req.Handle = types.String(value.(string))
+	}
+	if value, ok := d.GetOk("email"); ok {
+		req.Email = types.String(value.(string))
+	}
+
+	// Return if both handle and email are empty
+	if req.Handle == nil && req.Email == nil {
+		return diag.Errorf("either 'user_handle' or 'email' must be set in provider config")
+	}
+
+	// Invite requested member
+	_, r, err := client.APIClient.OrgMembers.Invite(ctx, orgHandle).Request(req).Execute()
+	if err != nil {
+		return diag.Errorf("error inviting member: %s", decodeResponse(r))
+	}
+	log.Printf("\n[DEBUG] Member invited: %v", decodeResponse(r))
+
+	/*
+	 * If a member is invited using user handle, use `OrgMembers.Get` to fetch the user details
+	 * If a member is invited using an email;
+	   * List the invited users, and find the requested user; if found return the requested user
+	   * else, list the accepted users, and find the requested user; if found return the requested user
+	 * TODO:: As of Dec 15, 2021, SDK doesn't support `email` in `OrgMembers.Get` API. If the API supports `email`, list operations can be ignored.
+	*/
+	var orgMemberDetails steampipe.OrgUser
+	if req.Handle != nil {
+		resp, r, err := client.APIClient.OrgMembers.Get(ctx, orgHandle, *req.Handle).Execute()
+		if err != nil {
+			if r.StatusCode == 404 {
+				return diag.Errorf("requested member %s not found", *req.Handle)
+			}
+			return diag.Errorf("error reading member %s.\nerr: %s", *req.Handle, decodeResponse(r))
+		}
+		orgMemberDetails = resp
+	} else {
+		data, err := listOrganizationMembersInvited(d, meta, req.Handle, req.Email)
+		if data.Id == "" {
+			data, err = listOrganizationMembersAccepted(d, meta, req.Handle, req.Email)
+		}
+
+		if err != nil {
+			return diag.Errorf("error fetching member from the list.\nerr: %s", decodeResponse(r))
+		}
+		orgMemberDetails = data
+	}
+
+	// Set property values
+	d.SetId(fmt.Sprintf("%s:%s", orgHandle, orgMemberDetails.UserHandle))
+	d.Set("user_handle", orgMemberDetails.UserHandle)
+	d.Set("created_at", orgMemberDetails.CreatedAt)
+	d.Set("email", orgMemberDetails.Email)
+	d.Set("association_id", orgMemberDetails.Id)
+	d.Set("organization_id", orgMemberDetails.OrgId)
+	d.Set("role", orgMemberDetails.Role)
+	d.Set("status", orgMemberDetails.Status)
+	d.Set("updated_at", orgMemberDetails.UpdatedAt)
+	d.Set("user_id", orgMemberDetails.UserId)
+	d.Set("version_id", orgMemberDetails.VersionId)
+
+	if orgMemberDetails.User != nil {
+		d.Set("display_name", orgMemberDetails.User.DisplayName)
+	}
+
+	return diags
+}
+
+func resourceOrganizationMemberRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*SteampipeClient)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	// Return if `organization` is not given in config
+	_, orgHandle := isUserConnection(client)
+	if orgHandle == "" {
+		return diag.Errorf("failed to get organization. Please set 'organization' in provider config")
+	}
+
+	id := d.Id()
+	idParts := strings.Split(id, ":")
+	if len(idParts) < 2 {
+		return diag.Errorf("unexpected format of ID (%q), expected <organization_handle>:<user_handle>", id)
+	}
+
+	if idParts[0] != orgHandle {
+		return diag.Errorf("given organization_handle %s does not match with the organization in the provider config %s", idParts[0], orgHandle)
+	}
+
+	if strings.Contains(idParts[1], "@") {
+		return diag.Errorf("invalid user_handle. Please provide valid user_handle to import")
+	}
+	userHandle := idParts[1]
+
+	resp, r, err := client.APIClient.OrgMembers.Get(context.Background(), orgHandle, userHandle).Execute()
+	if err != nil {
+		if r.StatusCode == 404 {
+			log.Printf("\n[WARN] Member (%s) not found", userHandle)
+			d.SetId("")
+			return nil
+		}
+		return diag.Errorf("error reading %s:%s.\nerr: %s", orgHandle, userHandle, decodeResponse(r))
+	}
+	log.Printf("\n[DEBUG] Organization Member received: %s", id)
+
+	d.SetId(id)
+	d.Set("user_handle", resp.UserHandle)
+	d.Set("created_at", resp.CreatedAt)
+	d.Set("email", resp.Email)
+	d.Set("association_id", resp.Id)
+	d.Set("organization_id", resp.OrgId)
+	d.Set("role", resp.Role)
+	d.Set("status", resp.Status)
+	d.Set("updated_at", resp.UpdatedAt)
+	d.Set("user_id", resp.UserId)
+	d.Set("version_id", resp.VersionId)
+
+	if resp.User != nil {
+		d.Set("display_name", resp.User.DisplayName)
+	}
+
+	return diags
+}
+
+func resourceOrganizationMemberUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*SteampipeClient)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	// Return if `organization` is not given in config
+	_, orgHandle := isUserConnection(client)
+	if orgHandle == "" {
+		return diag.Errorf("failed to get organization. Please set 'organization' in provider config")
+	}
+
+	userHandle := d.Get("user_handle").(string)
+	role := d.Get("role").(string)
+
+	// Create request
+	req := steampipe.UpdateOrgUserRequest{
+		Role: role,
+	}
+
+	log.Printf("\n[DEBUG] Updating membership: '%s:%s'", orgHandle, userHandle)
+
+	resp, r, err := client.APIClient.OrgMembers.Update(context.Background(), orgHandle, userHandle).Request(req).Execute()
+	if err != nil {
+		return diag.Errorf("error updating membership: %s", decodeResponse(r))
+	}
+	log.Printf("\n[DEBUG] Membership updated: %s:%s", orgHandle, resp.UserHandle)
+
+	// Update state file
+	id := fmt.Sprintf("%s:%s", orgHandle, resp.UserHandle)
+	d.SetId(id)
+	d.Set("user_handle", resp.UserHandle)
+	d.Set("created_at", resp.CreatedAt)
+	d.Set("email", resp.Email)
+	d.Set("association_id", resp.Id)
+	d.Set("organization_id", resp.OrgId)
+	d.Set("role", resp.Role)
+	d.Set("status", resp.Status)
+	d.Set("updated_at", resp.UpdatedAt)
+	d.Set("user_id", resp.UserId)
+	d.Set("version_id", resp.VersionId)
+
+	if resp.User != nil {
+		d.Set("display_name", resp.User.DisplayName)
+	}
+
+	return diags
+}
+
+func resourceOrganizationMemberDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*SteampipeClient)
+
+	// Warning or errors can be collected in a slice type
+	var diags diag.Diagnostics
+
+	// Return if `organization` is not given in config
+	_, orgHandle := isUserConnection(client)
+	if orgHandle == "" {
+		return diag.Errorf("failed to get organization. Please set 'organization' in provider config")
+	}
+
+	id := d.Id()
+	idParts := strings.Split(id, ":")
+	if len(idParts) < 2 {
+		return diag.Errorf("unexpected format of ID (%q), expected <organization_handle>:<user_handle>", id)
+	}
+	log.Printf("\n[DEBUG] Removing membership: %s", id)
+
+	_, r, err := client.APIClient.OrgMembers.Delete(context.Background(), orgHandle, idParts[1]).Execute()
+	if err != nil {
+		return diag.Errorf("error removing membership %s: %s", id, decodeResponse(r))
+	}
+	d.SetId("")
+
+	return diags
+}
+
+// List all the members who has been invited to the org.
+func listOrganizationMembersInvited(d *schema.ResourceData, meta interface{}, handle *string, email *string) (steampipe.OrgUser, error) {
+	client := meta.(*SteampipeClient)
+
+	// Return if `organization` is not given in config
+	_, orgHandle := isUserConnection(client)
+	if orgHandle == "" {
+		return steampipe.OrgUser{}, fmt.Errorf("failed to get organization. Please set 'organization' in provider config")
+	}
+
+	pagesLeft := true
+	var resp steampipe.ListOrgUsersResponse
+	var err error
+
+	for pagesLeft {
+		if resp.NextToken != nil {
+			resp, _, err = client.APIClient.OrgMembers.ListInvited(context.Background(), orgHandle).NextToken(*resp.NextToken).Execute()
+		} else {
+			resp, _, err = client.APIClient.OrgMembers.ListInvited(context.Background(), orgHandle).Execute()
+		}
+
+		if err != nil {
+			return steampipe.OrgUser{}, err
+		}
+
+		for _, i := range *resp.Items {
+			if (email != nil && i.Email == *email) || (handle != nil && i.UserHandle == *handle) {
+				return i, nil
+			}
+		}
+	}
+
+	return steampipe.OrgUser{}, nil
+}
+
+// List all the members who has accepted the request.
+func listOrganizationMembersAccepted(d *schema.ResourceData, meta interface{}, handle *string, email *string) (steampipe.OrgUser, error) {
+	client := meta.(*SteampipeClient)
+
+	// Return if `organization` is not given in config
+	_, orgHandle := isUserConnection(client)
+	if orgHandle == "" {
+		return steampipe.OrgUser{}, fmt.Errorf("failed to get organization. Please set 'organization' in provider config")
+	}
+
+	pagesLeft := true
+	var resp steampipe.ListOrgUsersResponse
+	var err error
+
+	for pagesLeft {
+		if resp.NextToken != nil {
+			resp, _, err = client.APIClient.OrgMembers.ListAccepted(context.Background(), orgHandle).NextToken(*resp.NextToken).Execute()
+		} else {
+			resp, _, err = client.APIClient.OrgMembers.ListAccepted(context.Background(), orgHandle).Execute()
+		}
+
+		if err != nil {
+			return steampipe.OrgUser{}, err
+		}
+
+		for _, i := range *resp.Items {
+			if (email != nil && i.Email == *email) || (handle != nil && i.UserHandle == *handle) {
+				return i, nil
+			}
+		}
+	}
+
+	return steampipe.OrgUser{}, nil
+}
---
+++ steampipecloud/resource_steampipecloud_organization_member_test.go
@@ -0,0 +1,155 @@
+// NOTE: Please provide a valid email in the config before performing the test
+
+package steampipecloud
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+// test suites
+func TestAccOrganizationMember_Basic(t *testing.T) {
+	orgHandle := "terraform" + randomString(3)
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOrganizationMemberDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOrganizationMemberConfig(orgHandle),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckMembershipOrganizationExists(orgHandle),
+					testAccCheckOrganizationMemberExists("steampipecloud_organization_member.test"),
+					resource.TestCheckResourceAttr(
+						"steampipecloud_organization_member.test", "role", "member"),
+				),
+			},
+			{
+				Config: testAccOrganizationMemberUpdateConfig(orgHandle),
+				Check: resource.TestCheckResourceAttr(
+					"steampipecloud_organization_member.test", "role", "owner"),
+			},
+		},
+	})
+}
+
+// configs
+func testAccOrganizationMemberConfig(orgHandle string) string {
+	return fmt.Sprintf(`
+provider "steampipecloud" {}
+
+resource "steampipecloud_organization" "test" {
+	handle = "%s"
+}
+
+provider "steampipecloud" {
+	alias        = "turbie"
+	organization = steampipecloud_organization.test.handle
+}
+
+# Please provide a valid email
+resource "steampipecloud_organization_member" "test" {
+	provider = steampipecloud.turbie
+	email    = "user@domain.com"
+	role     = "member"
+}`, orgHandle)
+}
+
+func testAccOrganizationMemberUpdateConfig(orgHandle string) string {
+	return fmt.Sprintf(`
+provider "steampipecloud" {}
+
+resource "steampipecloud_organization" "test" {
+	handle = "%s"
+}
+
+provider "steampipecloud" {
+  alias        = "turbie"
+  organization = steampipecloud_organization.test.handle
+}
+
+# Please provide a valid email
+resource "steampipecloud_organization_member" "test" {
+  provider = steampipecloud.turbie
+  email    = "user@domain.com"
+  role     = "owner"
+}`, orgHandle)
+}
+
+// helper functions
+func testAccCheckOrganizationMemberExists(resource string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		rs, ok := state.RootModule().Resources[resource]
+		if !ok {
+			return fmt.Errorf("not found: %s", resource)
+		}
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no Record ID is set")
+		}
+
+		// Extract organization handle and user handle from ID
+		id := rs.Primary.ID
+		idParts := strings.Split(id, ":")
+		if len(idParts) < 2 {
+			return fmt.Errorf("unexpected format of ID (%q), expected <organization_handle>:<user_handle>", id)
+		}
+
+		client := testAccProvider.Meta().(*SteampipeClient)
+		_, _, err := client.APIClient.OrgMembers.Get(context.Background(), idParts[0], idParts[1]).Execute()
+		if err != nil {
+			return fmt.Errorf("error fetching item with resource %s. %s", resource, err)
+		}
+		return nil
+	}
+}
+
+func testAccCheckOrganizationMemberDestroy(s *terraform.State) error {
+	client := testAccProvider.Meta().(*SteampipeClient)
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type == "steampipecloud_organization_member" {
+			// Extract organization handle and user handle from ID
+			id := rs.Primary.ID
+			idParts := strings.Split(id, ":")
+			if len(idParts) < 2 {
+				return fmt.Errorf("unexpected format of ID (%q), expected <organization_handle>:<user_handle>", id)
+			}
+
+			_, r, err := client.APIClient.OrgMembers.Get(context.Background(), idParts[0], idParts[1]).Execute()
+			if err == nil {
+				return fmt.Errorf("organization member still exists")
+			}
+
+			// If a organization is deleted, all the members will lost access to that organization
+			// If anyone try to get that deleted resource, it will always return `403 Forbidden` error
+			if r.StatusCode != 403 {
+				return fmt.Errorf("expected 'forbidden' error, got %s", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckMembershipOrganizationExists(orgHandle string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		client := testAccProvider.Meta().(*SteampipeClient)
+		ctx := context.Background()
+		var err error
+		var r *http.Response
+
+		// check if organization  is created
+		_, r, err = client.APIClient.Orgs.Get(ctx, orgHandle).Execute()
+		if err != nil {
+			if r.StatusCode != 403 {
+				return fmt.Errorf("error fetching organization with handle %s. %s", orgHandle, err)
+			}
+		}
+		return nil
+	}
+}
