df64fb59d3cfd2f22efed349ca8d1b3c07358e9f
LalitTurbot <lalit@turbot.com>
2021-12-21 16:26:53 +0530

    Update resource steampipecloud_connection to use config instead of individual configuration parameters.

--- steampipecloud/resource_steampipecloud_connection.go
+++ steampipecloud/resource_steampipecloud_connection.go
@@ -1,11 +1,8 @@
 package steampipecloud
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"regexp"
 	"strings"
@@ -13,7 +10,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/turbot/go-kit/helpers"
 	"github.com/turbot/go-kit/types"
 	steampipe "github.com/turbot/steampipe-cloud-sdk-go"
 )
@@ -67,168 +63,11 @@ func resourceConnection() *schema.Resource {
 				Optional: true,
 				Computed: true,
 			},
-
-			// Specific plugin configs arguments
-			// AWS Role Mode
-			"role_arn": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			// AWS Role Mode
-			"external_id": {
-				Type:         schema.TypeString,
-				Optional:     true,
-				ValidateFunc: validation.StringMatch(regexp.MustCompile(`^(?:u|o)_[a-z0-9]{20}:[a-z0-9]{8}$`), "External Id for the trusting role is invalid."),
-			},
-			// AWS, Alicloud
-			"access_key": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			// AWS, Alicloud
-			"secret_key": {
-				Type:      schema.TypeString,
-				Sensitive: true,
-				Optional:  true,
-			},
-			// AWS
-			"session_token": {
-				Type:      schema.TypeString,
-				Sensitive: true,
-				Optional:  true,
-			},
-			// GCP
-			"project": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			// GCP
-			"credentials": {
-				Type:      schema.TypeString,
-				Sensitive: true,
-				Optional:  true,
-			},
-			// AZURE, AzureAD
-			"environment": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			// AZURE, AzureAD
-			"tenant_id": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			// AZURE, AzureAD
-			"subscription_id": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			// AZURE, AzureAD
-			"client_id": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			// AZURE, AzureAD
-			"client_secret": {
-				Type:      schema.TypeString,
-				Sensitive: true,
-				Optional:  true,
-			},
-			// DigitalOcean, GitHub, Airtable, Jira, Linode, Slack
-			"token": {
-				Type:      schema.TypeString,
-				Sensitive: true,
-				Optional:  true,
-			},
-			// bearer_token
-			"bearer_token": {
-				Type:      schema.TypeString,
-				Sensitive: true,
-				Optional:  true,
-			},
-			// Airtable
-			"database_id": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			//  OCI
-			"user_ocid": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			//  OCI
-			"fingerprint": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			//  OCI
-			"tenancy_ocid": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			//  OCI
-			"private_key": {
-				Type:      schema.TypeString,
-				Sensitive: true,
-				Optional:  true,
-			},
-			// Jira, Bitbucket
-			"username": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			// Jira, Bitbucket
-			"base_url": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			// Bitbucket
-			"password": {
-				Type:      schema.TypeString,
-				Sensitive: true,
-				Optional:  true,
-			},
-			// Hacker News
-			"max_items": {
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
-			// hackernews, Zendesk, DataDog, IBM, Cloudflare, Stripe
-			"api_key": {
-				Type:      schema.TypeInt,
-				Sensitive: true,
-				Optional:  true,
-			},
-			// Zendesk
-			"subdomain": {
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
-			// Zendesk, Cloudflare
-			"email": {
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
-			// Turbot
-			"workspace": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			// AWS, OCI, Alicloud, IBM
-			"regions": {
-				Type:     schema.TypeList,
-				Optional: true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
-			},
-			// Airtable
-			"tables": {
-				Type:     schema.TypeList,
-				Optional: true,
-				Elem: &schema.Schema{
-					Type: schema.TypeString,
-				},
+			"config": {
+				Type:             schema.TypeString,
+				Optional:         true,
+				ValidateFunc:     validation.StringIsJSON,
+				DiffSuppressFunc: suppressIfDataMatches,
 			},
 		},
 	}
@@ -237,8 +76,9 @@ func resourceConnection() *schema.Resource {
 func resourceConnectionCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	// Warning or errors can be collected in a slice type
 	var diags diag.Diagnostics
-	var plugin, connHandle string
+	var plugin, connHandle, configString string
 	var config map[string]interface{}
+	var err error
 
 	if value, ok := d.GetOk("handle"); ok {
 		connHandle = value.(string)
@@ -247,19 +87,9 @@ func resourceConnectionCreate(ctx context.Context, d *schema.ResourceData, meta 
 		plugin = value.(string)
 	}
 
-	// Get config to create connection
-	connConfig, err := CreateConnectionConfiguration(d)
-	if err != nil {
-		return diag.Errorf("resourceConnectionUpdate. Error while creating connection:  %v", err)
-	}
-
-	configByteData, err := json.Marshal(connConfig)
-	if err != nil {
-		return diag.Errorf("resourceConnectionCreate. Marshalling connection config error  %v", err)
-	}
-	err = json.Unmarshal(configByteData, &config)
-	if err != nil {
-		return diag.Errorf("resourceConnectionCreate. Unmarshalling connection config error  %v", err)
+	// save the formatted data: this is to ensure the acceptance tests behave in a consistent way regardless of the ordering of the json data
+	if value, ok := d.GetOk("config"); ok {
+		configString, config = formatJson(value.(string))
 	}
 
 	req := steampipe.CreateConnectionRequest{
@@ -286,7 +116,6 @@ func resourceConnectionCreate(ctx context.Context, d *schema.ResourceData, meta 
 	} else {
 		resp, r, err = client.APIClient.OrgConnections.Create(ctx, orgHandle).Request(req).Execute()
 	}
-
 	if err != nil {
 		return diag.Errorf("resourceConnectionCreate. Create connection api error  %v", decodeResponse(r))
 	}
@@ -298,18 +127,14 @@ func resourceConnectionCreate(ctx context.Context, d *schema.ResourceData, meta 
 	d.Set("plugin", resp.Plugin)
 	d.Set("created_at", resp.CreatedAt)
 	d.Set("updated_at", resp.UpdatedAt)
-	d.SetId(resp.Handle)
-	// Save the config
-	if resp.Config != nil {
-		for k, v := range *resp.Config {
-			if v != nil {
-				if helpers.StringSliceContains([]string{"regions", "Regions", "tables"}, k) {
-					d.Set(strings.ToLower(k), v.([]interface{}))
-				} else {
-					d.Set(k, v.(string))
-				}
-			}
-		}
+	if config != nil {
+		d.Set("config", configString)
+	}
+
+	if strings.HasPrefix(resp.IdentityId, "o_") {
+		d.SetId(fmt.Sprintf("%s:%s", orgHandle, resp.Handle))
+	} else {
+		d.SetId(resp.Handle)
 	}
 
 	return diags
@@ -319,17 +144,27 @@ func resourceConnectionRead(ctx context.Context, d *schema.ResourceData, meta in
 	client := meta.(*SteampipeClient)
 
 	// Warning or errors can be collected in a slice type
+	var connectionHandle, orgHandle string
 	var diags diag.Diagnostics
 	var err error
 	var r *http.Response
 	var resp steampipe.Connection
+	var isUser = false
+	id := d.Id()
 
-	connectionHandle := d.Id()
+	ids := strings.Split(id, ":")
+	if len(ids) == 2 {
+		orgHandle = strings.Split(id, ":")[0]
+		connectionHandle = strings.Split(id, ":")[1]
+	} else if len(ids) == 1 {
+		isUser = true
+		connectionHandle = strings.Split(id, ":")[0]
+	}
+
 	if connectionHandle == "" {
 		return diag.Errorf("resourceConnectionRead. Connection handle not present.")
 	}
 
-	isUser, orgHandle := isUserConnection(d)
 	if isUser {
 		var actorHandle string
 		actorHandle, r, err = getUserHandler(ctx, client)
@@ -361,23 +196,18 @@ func resourceConnectionRead(ctx context.Context, d *schema.ResourceData, meta in
 	d.Set("handle", resp.Handle)
 	d.Set("created_at", resp.CreatedAt)
 	d.Set("updated_at", resp.UpdatedAt)
-	if resp.Config != nil {
-		for k, v := range *resp.Config {
-			if v != nil {
-				if helpers.StringSliceContains([]string{"regions", "Regions", "tables"}, k) {
-					d.Set(strings.ToLower(k), v.([]interface{}))
-				} else {
-					d.Set(k, v.(string))
-				}
-			}
-		}
-	}
 
 	return diags
 }
 
 func resourceConnectionUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*SteampipeClient)
+
+	var configString string
+	var r *http.Response
+	var resp steampipe.Connection
+	var err error
+	var config map[string]interface{}
 
 	// Warning or errors can be collected in a slice type
 	var diags diag.Diagnostics
@@ -387,23 +217,9 @@ func resourceConnectionUpdate(ctx context.Context, d *schema.ResourceData, meta 
 		return diag.Errorf("handle must be configured")
 	}
 
-	var r *http.Response
-	var resp steampipe.Connection
-	var err error
-	var config map[string]interface{}
-
-	// Create connection config to be updated
-	connConfig, err := CreateConnectionConfiguration(d)
-	if err != nil {
-		return diag.Errorf("resourceConnectionUpdate. Error while creating connection:  %v", err)
-	}
-	data, err := json.Marshal(connConfig)
-	if err != nil {
-		return diag.Errorf("resourceConnectionUpdate. Marshalling connection config error  %v", err)
-	}
-	err = json.Unmarshal(data, &config)
-	if err != nil {
-		return diag.Errorf("resourceConnectionUpdate. Unmarshalling connection config error  %v", err)
+	// save the formatted data: this is to ensure the acceptance tests behave in a consistent way regardless of the ordering of the json data
+	if value, ok := d.GetOk("config"); ok {
+		configString, config = formatJson(value.(string))
 	}
 
 	req := steampipe.UpdateConnectionRequest{Handle: types.String(newConnectionHandle.(string))}
@@ -434,17 +250,13 @@ func resourceConnectionUpdate(ctx context.Context, d *schema.ResourceData, meta 
 	d.Set("created_at", resp.CreatedAt)
 	d.Set("updated_at", resp.UpdatedAt)
 	d.Set("plugin", *resp.Plugin)
-	d.SetId(resp.Handle)
-	if resp.Config != nil {
-		for k, v := range *resp.Config {
-			if v != nil {
-				if helpers.StringSliceContains([]string{"regions", "Regions", "tables"}, k) {
-					d.Set(strings.ToLower(k), v.([]interface{}))
-				} else {
-					d.Set(k, v.(string))
-				}
-			}
-		}
+	if strings.HasPrefix(resp.IdentityId, "o_") {
+		d.SetId(fmt.Sprintf("%s:%s", orgHandle, resp.Handle))
+	} else {
+		d.SetId(resp.Handle)
+	}
+	if config != nil {
+		d.Set("config", configString)
 	}
 	return diags
 }
@@ -483,138 +295,14 @@ func resourceConnectionDelete(ctx context.Context, d *schema.ResourceData, meta 
 	return diags
 }
 
-func CreateConnectionConfiguration(d *schema.ResourceData) (ConnectionConfig, error) {
-	var connConfig ConnectionConfig
-
-	if value, ok := d.GetOk("access_key"); ok {
-		connConfig.AccessKey = value.(string)
-	}
-	if value, ok := d.GetOk("api_key"); ok {
-		connConfig.ApiKey = value.(string)
-	}
-	if value, ok := d.GetOk("base_url"); ok {
-		connConfig.BaseURL = value.(string)
-	}
-	if value, ok := d.GetOk("bearer_token"); ok {
-		connConfig.BearerToken = value.(string)
-	}
-	if value, ok := d.GetOk("client_id"); ok {
-		connConfig.ClientID = value.(string)
-	}
-	if value, ok := d.GetOk("client_secret"); ok {
-		connConfig.ClientSecret = value.(string)
-	}
-	if value, ok := d.GetOk("credentials"); ok {
-		creds := value.(string)
-		buffer := new(bytes.Buffer)
-		if err := json.Compact(buffer, []byte(creds)); err != nil {
-			log.Println(err)
-		}
-		connConfig.Credentials = buffer.String()
-	}
-	if value, ok := d.GetOk("database_id"); ok {
-		connConfig.DatabaseID = value.(string)
-	}
-	if value, ok := d.GetOk("email"); ok {
-		connConfig.Email = value.(string)
-	}
-	if value, ok := d.GetOk("environment"); ok {
-		connConfig.Environment = value.(string)
-	}
-	if value, ok := d.GetOk("fingerprint"); ok {
-		connConfig.Fingerprint = value.(string)
-	}
-	if value, ok := d.GetOk("max_items"); ok {
-		connConfig.MaxItems = value.(int)
-	}
-	if value, ok := d.GetOk("password"); ok {
-		connConfig.Password = value.(string)
-	}
-	if value, ok := d.GetOk("private_key"); ok {
-		privateKey := value.(string)
-		connConfig.PrivateKey = strings.ReplaceAll(privateKey, "\r\n", "\\n")
-	}
-	if value, ok := d.GetOk("project"); ok {
-		connConfig.Project = value.(string)
-	}
-	if value, ok := d.GetOk("regions"); ok {
-		var regions []string
-		for _, item := range value.([]interface{}) {
-			regions = append(regions, item.(string))
-		}
-		connConfig.Regions = regions
-	}
-	if value, ok := d.GetOk("secret_key"); ok {
-		connConfig.SecretKey = value.(string)
-	}
-	if value, ok := d.GetOk("session_token"); ok {
-		connConfig.SessionToken = value.(string)
-	}
-	if value, ok := d.GetOk("subdomain"); ok {
-		connConfig.Subdomain = value.(string)
-	}
-	if value, ok := d.GetOk("subscription_id"); ok {
-		connConfig.SubscriptionID = value.(string)
-	}
-	if value, ok := d.GetOk("tables"); ok {
-		var tables []string
-		for _, item := range value.([]interface{}) {
-			tables = append(tables, item.(string))
-		}
-		connConfig.Tables = tables
-	}
-	if value, ok := d.GetOk("tenancy_ocid"); ok {
-		connConfig.TenancyOCID = value.(string)
-	}
-	if value, ok := d.GetOk("tenant_id"); ok {
-		connConfig.TenantID = value.(string)
-	}
-	if value, ok := d.GetOk("token"); ok {
-		connConfig.Token = value.(string)
-	}
-	if value, ok := d.GetOk("user_ocid"); ok {
-		connConfig.UserOCID = value.(string)
-	}
-	if value, ok := d.GetOk("role_arn"); ok {
-		connConfig.RoleArn = value.(string)
-	}
-	if value, ok := d.GetOk("external_id"); ok {
-		connConfig.ExternalID = value.(string)
-	}
-	if value, ok := d.GetOk("workspace"); ok {
-		connConfig.Workspace = value.(string)
+// config is a json string
+// apply standard formatting to old and new data then compare
+func suppressIfDataMatches(k, old, new string, d *schema.ResourceData) bool {
+	if old == "" || new == "" {
+		return false
 	}
 
-	return connConfig, nil
-}
-
-type ConnectionConfig struct {
-	AccessKey      string   `json:"access_key,omitempty"`
-	ApiKey         string   `json:"api_key,omitempty"`
-	RoleArn        string   `json:"role_arn,omitempty"`
-	ExternalID     string   `json:"external_id,omitempty"`
-	BaseURL        string   `json:"base_url,omitempty"`
-	BearerToken    string   `json:"bearer_token,omitempty"`
-	ClientID       string   `json:"client_id,omitempty"`
-	ClientSecret   string   `json:"client_secret,omitempty"`
-	Credentials    string   `json:"credentials,omitempty"`
-	DatabaseID     string   `json:"database_id,omitempty"`
-	Email          string   `json:"email,omitempty"`
-	Environment    string   `json:"environment,omitempty"`
-	Fingerprint    string   `json:"fingerprint,omitempty"`
-	MaxItems       int      `json:"max_items,omitempty"`
-	Password       string   `json:"password,omitempty"`
-	PrivateKey     string   `json:"private_key,omitempty"`
-	Project        string   `json:"project,omitempty"`
-	Regions        []string `json:"regions,omitempty"`
-	SecretKey      string   `json:"secret_key,omitempty"`
-	SessionToken   string   `json:"session_token,omitempty"`
-	Subdomain      string   `json:"subdomain,omitempty"`
-	SubscriptionID string   `json:"subscription_id,omitempty"`
-	Tables         []string `json:"tables,omitempty"`
-	TenancyOCID    string   `json:"tenancy_ocid,omitempty"`
-	TenantID       string   `json:"tenant_id,omitempty"`
-	Token          string   `json:"token,omitempty"`
-	UserOCID       string   `json:"user_ocid,omitempty"`
-	Workspace      string   `json:"workspace,omitempty"`
+	oldFormatted, _ := formatJson(old)
+	newFormatted, _ := formatJson(new)
+	return oldFormatted == newFormatted
 }
--- steampipecloud/utils.go
+++ steampipecloud/utils.go
@@ -1,12 +1,15 @@
 package steampipecloud
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"math/rand"
 	"net/http"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/turbot/go-kit/helpers"
 )
 
 // isUserConnection:: Check if the connection is scoped on an user or a specific organization
@@ -48,4 +51,54 @@ func randomString(n int) string {
 		b[i] = letterBytes[rand.Intn(len(letterBytes))]
 	}
 	return string(b)
+}
+
+// apply standard formatting to a json string by unmarshalling into a map then marshalling back to JSON
+func formatJson(body string) (string, map[string]interface{}) {
+	buffer := new(bytes.Buffer)
+	err := json.Compact(buffer, []byte(body))
+	if err != nil {
+		return body, nil
+	}
+	data := map[string]interface{}{}
+	if err := json.Unmarshal(buffer.Bytes(), &data); err != nil {
+		// ignore error and just return original body
+		return body, nil
+	}
+	for k, v := range data {
+		if helpers.StringSliceContains([]string{"credentials"}, k) {
+			bufferCredentials := new(bytes.Buffer)
+			if compactErr := json.Compact(bufferCredentials, []byte(v.(string))); compactErr != nil {
+				continue
+			}
+			data[k] = bufferCredentials.String()
+		} else if k == "private_key" {
+			privateKey := v.(string)
+			data[k] = strings.ReplaceAll(privateKey, "\r\n", "\\n")
+		}
+	}
+
+	body, err = mapToJsonString(data)
+	if err != nil {
+		// ignore error and just return original body
+		return body, data
+	}
+	return body, data
+}
+
+func mapToJsonString(data map[string]interface{}) (string, error) {
+	dataBytes, err := json.MarshalIndent(data, "", " ")
+	if err != nil {
+		return "", err
+	}
+	jsonData := string(dataBytes)
+	return jsonData, nil
+}
+
+func JsonStringToMap(dataString string) (map[string]interface{}, error) {
+	var data = make(map[string]interface{})
+	if err := json.Unmarshal([]byte(dataString), &data); err != nil {
+		return nil, err
+	}
+	return data, nil
 }
