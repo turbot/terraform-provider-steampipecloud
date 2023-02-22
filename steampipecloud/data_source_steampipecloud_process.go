5d1d81fb3e02fd23ae119f2c90e1d76873e39f8a
Siddhartha Das <siddhartha@turbot.com>
2023-02-22 18:19:16 +0530

    Update steampipecloud/data_source_steampipecloud_process.go
    Co-authored-by: Ved misra <47312748+misraved@users.noreply.github.com>

--- steampipecloud/data_source_steampipecloud_process.go
+++ steampipecloud/data_source_steampipecloud_process.go
@@ -95,7 +95,7 @@ func dataSourceProcessRead(ctx context.Context, d *schema.ResourceData, meta int
 			return diag.Errorf("dataSourceProcessRead.getUserHandler error  %v", decodeResponse(r))
 		}
 		log.Printf("\n[DEBUG] Process get context-> identity:'%s'; workspace:'%s'; process:'%s'", actorHandle, workspace, processId)
-		// If a workspace is not passed we can assume its an identity process
+		// If a workspace is not passed we can assume that it is an identity process
 		if workspace == "" {
 			resp, r, err = client.APIClient.UserProcesses.Get(ctx, actorHandle, processId).Execute()
 		} else {
